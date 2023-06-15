d25366b482b8f77e244f6314c41717fb0ed10670
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-15 13:16:56 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-	{char str[] = "Programming is like building a multilingual puzzle\n";
+	{char str[] = "\"Programming is like building a multilingual puzzle";
 		puts(str);
 		return (0); }
