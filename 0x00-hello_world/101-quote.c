02a95b9841099662365c0bf0edc9126959afa922
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-15 16:37:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 /**
  * main - Entry point
- * Description: 'write'
- * Return: Always 1 (Success)
+ * Description: 'quote'
+ * Return: Always  (Success)
  */
 int main(void)
-	{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1); }
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1); }
