c88d86757c843aa4be146c4c351aa20b45e32edb
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-16 11:56:28 -0700

    skip

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,20 +6,20 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-        {char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	{char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
 
-        int length = strlen(alphabets);
+	int length = strlen(alphabets);
 
-        int start = 0;
+	int start = 0;
 
-        do {
-	if (alphabets[start] == 'q' || alphabets[start] == 'e' )
+	do {
+	if (alphabets[start] == 'q' || alphabets[start] == 'e')
 	{
 	start++;
-	 continue;
+	continue;
 	}
-        putchar(alphabets[start]);
-        start++;
-        } while (start < length);
-        putchar('\n');
-        return (0); }
+	putchar(alphabets[start]);
+	start++;
+	} while (start < length);
+	putchar('\n');
+	return (0); }
