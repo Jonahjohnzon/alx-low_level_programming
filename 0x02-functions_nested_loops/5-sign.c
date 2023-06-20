ef31af97c8d2dafd9a55114ae1d904026b09f64a
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-20 15:50:18 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,19 +10,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
-	_putchar(n);
+	_putchar('0' + n);
 	return (1);
 	}
 	else if (n < 0)
 	{
 	_putchar('-');
-	_putchar(n);
+	_putchar('0' + n);
 	return (-1);
 	}
 	else
 	{
 	_putchar(0);
-	_putchar(n);
+	_putchar('0' + n);
 	return (0);
 	}
 }
