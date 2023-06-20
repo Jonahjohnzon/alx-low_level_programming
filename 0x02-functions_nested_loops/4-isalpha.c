4cb904fa8ac871349ac5c4d65c4b887892b3858f
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-20 14:41:25 -0700

    check alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,16 +3,16 @@
  * _isalpha - Check case
  * @c: Character to be checked
  * Description: 'Check for case difference'
- * Return: 1 or 2
+ * Return: 1 or 0
  */
 int _isalpha(int c)
 {
-	if (_isalpha(c))
+	if (_isalpha(c) == 0)
 	{
-	return (1);
+	return (0);
 	}
 	else
 	{
-	return (0);
+	return (1);
 	}
 }
