c085439c95299ba14a53f5827ef6ad86d3b99e53
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-20 05:47:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'Putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char stringss[] = "_putchar";
+
+	int length = strlen(stringss);
+
+	int count;
+
+	for (count = 0; count <= length; count++)
+	{
+	putchar(stringss[count]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0-putchar.c : This is a program that prints _putchar, followed by a new line.
