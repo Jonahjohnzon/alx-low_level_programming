26caf5910b048048acde5562572dc206caa57e50
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-20 15:37:41 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Check sign
+ * @n: Number to be checked
+ * Description: 'Check for sign difference'
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	return (1);
+	}
+	else if (n < 0)
+	{
+	return (-1);
+	}
+	else
+	{
+	return (0);
+	}
+}
