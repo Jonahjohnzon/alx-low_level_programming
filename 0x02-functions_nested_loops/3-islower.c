9c600d7cd9ef6d911b02515023a1a0b4b0bcbfda
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-20 14:17:31 -0700

    check case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * _islower - Check case
+ * @c: Character to be checked
+ * Description: 'Check for case difference'
+ * Return: 1 or 2
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
