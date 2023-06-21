ffa48d029c1345ce012102bf7f2e6e5a62f29e52
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-21 03:49:59 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,11 +28,8 @@ void print_times_table(int n)
 		{
 		_putchar(',');
 		_putchar(' ');
-		if (third < 10)
-		{
 		_putchar(' ');
-		}
-		if (third < 100)
+		if (third < 10)
 		{
 		_putchar(' ');
 		}
