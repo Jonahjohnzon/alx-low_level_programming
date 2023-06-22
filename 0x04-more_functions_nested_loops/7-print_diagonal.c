308d0b63b442f6c0a20bca75e5563734188cd34c
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-22 14:08:24 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - Print line
+ * Description: 'Print line according to input'
+ * @n: Input
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int first;
+
+	int space;
+
+	if (n != 0 && n > 0)
+	{
+	for (first = 1; first <= n; first++)
+	{
+	for (space = 1; space <= first; space++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\');
+	_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
