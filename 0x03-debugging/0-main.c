e819f363baa64a3d79c5e9b270f1cc5ac5fda6c0
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-22 04:51:50 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'Test a function'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
