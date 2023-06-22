073cea13f44f01ae87dc7651531edc8deeedbcc6
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-22 13:08:04 -0700

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 
 	for (; number <= 14; number++)
 	{
-	_putchar(number + '0');
+	if (number > 9)
+	{
+	_putchar((number / 10) + '0');
+	}
+	_putchar((number % 10) + '0');
 	}
 	_putchar('\n');
 	}
