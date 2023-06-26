15515a36353df2fcdff1b81ddbbab375f85e7893
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-26 07:52:50 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - pointers and string
+ * Description: 'prints a string, followed by a new line, to stdout'
+ * @str: String
+ * Return: void
+ */
+void _puts(char *str)
+{	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
