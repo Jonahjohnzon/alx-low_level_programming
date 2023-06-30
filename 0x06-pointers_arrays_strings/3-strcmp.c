9288f956cf058a165f21acc9a3f2dc69a2897a8d
jonah ominyi <jonahjohnzon@gmail.com>
2023-06-30 02:29:37 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strcmp - Pointers and string
+ * Description: Comparing two string
+ * @s1: input
+ * @s2: input
+ * Return: numbers
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	int z = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[z] != '\0')
+	{
+		z++;
+	}
+	if (i == z)
+	{
+		return (0);
+	}
+	else if (i > z)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ README
 0-strcat.c : Write a function that concatenates two strings.
 1-strncat.c : Write a function that concatenates two strings.
 2-strncpy.c : Write a function that copies a string.
+3-strcmp.c : Write a function that compares two strings
