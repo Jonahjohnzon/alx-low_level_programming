b8f8e2f005090e3f5f263ed4d68856eafc90a36d
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-06 08:14:56 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+int count(int x, int y);
+
+/**
+ * _sqrt_recursion - Recursion
+ * Description: 'function that returns the natural square root of a number'
+ * @n: input
+ * Return: int (Success)
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (count(n, 0));
+}
+/**
+ * count - Recursion
+ * Description: 'function that returns the natural square root of a number'
+ * @x: input
+ * @y: input
+ * Return: int (Success)
+ */
+int count(int x, int y)
+{
+	if (y * y > x)
+	{
+		return (-1);
+	}
+	if (y * y == x)
+	{
+		return (y);
+	}
+	count(n, y + 1);
+}
