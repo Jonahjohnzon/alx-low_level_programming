126339a546def08268ad0295c71e050869f73243
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-06 01:37:18 -0700

    rev-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _puts_recursion - Recursion
+ * _print_rev_recursion - Recursion
  * Description: 'Write a function that prints a string, followed by a new line'
  * @s: input
  * Return void
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
@@ -15,7 +15,7 @@ void _puts_recursion(char *s)
 	{
 	s++;
 
-	_puts_recursion(s);
+	_print_rev_recursion(s);
 	_putchar(*s);
 	}
 }
