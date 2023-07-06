0d89bc4c039e61a103f905e15bf5891c8ee9e73b
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-06 02:40:49 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		return (0);
 	}
-	int x = 1 + _strlen_recursion(s + 1);
 
-	return (x);
+	return (1 + _strlen_recursion(s + 1));
 }
