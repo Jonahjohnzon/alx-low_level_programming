3433f228bf6cdd417d0c69be97989a997d9f8079
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-06 12:44:57 -0700

    print nummber

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+int prime(int x, int y);
+/**
+ * is_prime_number - Recursion
+ * Description: 'function that returns int'
+ * @n: input
+ * Return: int (Success)
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+	{
+	return (0);
+	}
+	prime(n, n - 1)
+}
+/**
+ * prime - Recursion
+ * Description: 'function that returns prime or not'
+ * @x: input
+ * @y: input
+ * Return: int (Success)
+ */
+int prime(int x, int y)
+{
+	if (y == 1)
+	{
+	return (1);
+	}
+	if (x % y == 0 && y > 0)
+	{
+	return (0);
+	}
+	prime(x, y - 1);
+}
