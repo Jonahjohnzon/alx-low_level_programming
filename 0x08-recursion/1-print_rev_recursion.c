ace2c0db55462c04954f474854fc54c62233767e
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-06 01:46:41 -0700

    rev-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,17 +5,11 @@
  * @s: input
  * Return void
  */
-void _print_rev_recursion(char *s)
-{
-	if (*s == '\0')
+void _print_rev_recursion(char *s}
+{	
+	if (*s)
 	{
-		return;
-	}
-	else
-	{
-	s++;
-
-	_print_rev_recursion(s);
-	_putchar(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
