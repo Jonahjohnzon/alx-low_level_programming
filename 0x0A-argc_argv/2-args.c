9121b8df7ce26aae884c304689a9a98426eaad3b
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-10 05:14:26 -0700

    print arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the programs and arg
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Description: 'print argv'
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+	printf("%s\n", *argv[count]);
+	}
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,4 @@
 argc
 0-whatsmyname.c : Write a program that prints its name, followed by a new line
+1-args.c : Program that prints the number of arguments passed into it.
+2-args.c : Program that prints all arguments it receives
