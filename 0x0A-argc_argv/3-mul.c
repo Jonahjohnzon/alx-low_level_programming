6060090399fbc0ee70d60f63a52059caffd0c429
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-10 09:11:13 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,17 +5,17 @@
  * main - prints the name of the programs and arg
  * @argc: number of arguments
  * @argv: array of arguments
- * Description: 'print addition argv'
+ * Description: 'print additioin argv'
  * Return: 0 or 1 (Success)
  */
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	};
-	printf("%d\n", _atoi(argv[1]) + _atoi(argv[2]));
+	printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
 
 	return (0);
 }
