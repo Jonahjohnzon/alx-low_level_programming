3eab819a0eba979e0a083b1df1fd6baa0b326fdb
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-10 09:07:14 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the programs and arg
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Description: 'print addition argv'
+ * Return: 0 or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	};
+	printf("%d\n", _atoi(argv[1]) + _atoi(argv[2]));
+
+	return (0);
+}
