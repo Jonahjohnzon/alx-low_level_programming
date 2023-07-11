0a5c25b08e5784b4c34f8db43c6b074bc572a07e
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-11 08:32:19 -0700

    array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#define MAIN
+
+/**
+ * *_strdup - malloc
+ * Description: ' function that creates an array of chars'
+ * @str: input
+ * Return: Pointer or NULL
+ */
+char *_strdup(char *str)
+{
+	char *addr;
+
+	int count, alpha;
+
+	alpha = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + alpha) != '\0')
+	{
+		alpha++;
+	}
+	addr = (char *) malloc(alpha * sizeof(char));
+
+	if (addr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; count <= alpha; count++)
+	{
+		*(addr + count) = *(str + count);
+	}
+
+
+	return (addr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,5 @@
 #ifdef MAIN
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+char *_strdup(char *str);
 #endif
