fd56aea839d2c3a300e7a7bbba500822468f5671
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-11 09:26:30 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#define MAIN
+
+/**
+ * *str_concat - malloc
+ * Description: ' function that creates an array of chars'
+ * @s1: input
+ * @s2: input
+ * Return: Pointer or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *addr;
+
+	unsigned int count, alpha, alpha2, count2;
+
+	alpha = 0;
+
+	alpha2 = 0;
+
+	while (s1 != NULL && *(s1 + alpha) != '\0')
+	{
+		alpha++;
+	}
+
+	while (s2 != NULL && *(s2 + alpha2) != '\0')
+	{
+		alpha2++;
+	}
+	addr = (char *) malloc((alpha + alpha2 + 1) * sizeof(char));
+
+	if (addr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; count < alpha; count++)
+	{
+		addr[count] = s1[count];
+	}
+	for (count2 = 0; count2 < alpha2; count2++)
+	{
+	addr[count] = s1[count2];
+	}
+
+	*(addr + count) = '\0';
+
+	return (addr);
+}
