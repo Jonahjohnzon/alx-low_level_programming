bd327ea22385aafb89a6c10a01afa4be67df2092
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-13 08:03:18 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - malloc
+ * Description: 'Function that allocates memory using malloc'
+ * @b: input
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = (unsigned int *) malloc(sizeof(b));
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifdef MAIN
+#define MAIN
+void *malloc_checked(unsigned int b);
+#endif
