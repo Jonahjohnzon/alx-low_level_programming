b416c8f4c301aacf4fdb47fc1d49ded4c9c6e74e
jonah ominyi <jonahjohnzon@gmail.com>
2023-07-18 05:46:53 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dogtype - typedef for struct dog
+ */
+typedef dog dogtype;
