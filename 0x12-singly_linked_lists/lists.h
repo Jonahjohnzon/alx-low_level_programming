e787b098fab31808b3e09f06b3125e4ed6d8e102
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-07-27 09:08:57 -0700

    print

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct list_s - singly linked
+ * @str: string
+ * @len: length
+ * @next: point
+ * Description: singly linke list
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
