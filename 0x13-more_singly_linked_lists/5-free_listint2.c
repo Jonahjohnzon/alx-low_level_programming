367d366202296aef25975ed0adbcdca8daa470a3
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-01 12:13:14 -0700

    Free malloc

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - free linked list
+ * @head: listint_t input
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+	
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
+
+}
