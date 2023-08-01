7171ff5d1d3b91675653cadc5a90a7d9cd4076a7
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-01 15:00:48 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * free_listint_safe - free node
+ * @h: input
+ * Return: size_t
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t leng = 0;
+	int diff;
+	listint_t *tmp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			leng++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			leng++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (leng);
+}
