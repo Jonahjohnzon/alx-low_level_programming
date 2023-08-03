17e4454c3ebc29b3e9a746d543562e93863d2912
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-03 09:00:47 -0700

    binary to base 10

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - Recursion
+ * Description: 'function that returns the'
+ * @x: input
+ * @y: input
+ * Return: int (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+	return (1);
+	}
+	if (y < 0)
+	{
+	return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
+/**
+ * binary_to_uint - Convert to base 10
+ * @b: base 2
+ * Return: int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int count;
+
+	unsigned int amount = 0;
+
+	int count_total = malloc(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	if (count_total == NULL)
+	{
+		return (0);
+	}
+	for (count = 0; count < count_total; count++)
+	{
+		if (*b != '0' || *b != '1')
+		{
+			return (0);
+		}
+		if (*b == '0')
+		{
+		amount += (0) * (_pow_recursion(10, count));
+		}
+		else if (*b == '1')
+		{
+		amount += 1 * (_pow_recursion(10, count));
+		}
+		b++;
+
+	}
+	return (amount);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _pow_recursion(int x, int y);
+unsigned int binary_to_uint(const char *b);
+#endif
