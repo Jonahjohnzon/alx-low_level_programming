3ba3c28f46f75de419854f7aa2ee4ed3f83bc6f8
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-03 14:54:47 -0700

    binary print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * base - loop
+ * @n: input
+ */
+void base(unsigned long int n)
+{
+	int bases;
+
+	if (n == 0)
+	{
+	return;
+	}
+	base(n / 2);
+	bases = n % 2;
+	_putchar(bases + '0');
+}
+/**
+ * print_binary - print binary
+ * @n: base 10
+ */
+void print_binary(unsigned long int n)
+
+	if (n == 0)
+	{
+		_putchar('0' + 0);
+	}
+	else if (n == 1)
+	{
+		_putchar('0' + 1);
+	}
+	else
+	{
+		base(n);
+	}
+
+}
