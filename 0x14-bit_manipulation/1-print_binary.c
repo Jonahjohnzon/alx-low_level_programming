9bc1270e0d77142d3ab9cd5a154c85b2fd67d9ec
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-03 15:24:21 -0700

    bit manipulations

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,28 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * base - loop
- * @n: input
- */
-void base(unsigned long int n)
-{
-	int bases;
-
-	if (n == 0)
-	{
-	return;
-	}
-	base(n / 2);
-	bases = n % 2;
-	_putchar(bases + '0');
-}
 /**
  * print_binary - print binary
  * @n: base 10
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int tmp;
+	int shift;
+
 	if (n == 0)
 	{
-		_putchar('0' + 0);
-	}
-	else if (n == 1)
-	{
-		_putchar('0' + 1);
-	}
-	else
-	{
-		base(n);
+		printf("0");
+		return;
 	}
 
+	for (tmp = n, shift = 0; (tmp >>= 1) > 0; shift++)
+		;
+
+	for (; shift >= 0; shift--)
+	{
+		if ((n >> shift) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
 }
