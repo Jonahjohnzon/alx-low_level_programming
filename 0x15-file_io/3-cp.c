50154fa64258c63cf2cc04cc133009817f1bb4f8
jonahjohnzon <Jonahjohnzon@gmail.com>
2023-08-08 08:38:59 -0700

    cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	bufr = create_buffer(argv[2]);
+	buff = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buff, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
