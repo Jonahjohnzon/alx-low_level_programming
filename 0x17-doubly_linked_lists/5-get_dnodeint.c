897002eb4ce273b2e83a690dc4c36a0693fef7a1
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-14 13:19:52 -0700

    get node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function
+ * Description: get  index in list
+ * @head: pointer
+ * @index: number
+ * Return: list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head->prev != NULL)
+	{
+		head = head->prev;
+	}
+	while (head != NULL)
+	{
+		if (count = index)
+		{
+			break;
+		}
+		head = head->next;
+		count++;
+	}
+	return (head)
+}
