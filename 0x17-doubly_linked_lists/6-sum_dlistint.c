2c6edb68e257c2ce8c554669ce2f6c4c55d8dc19
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-14 13:36:18 -0700

    add node

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function
+ * @head: pointer
+ * Description: sum numbers in list
+ * Return: int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *list_h = head;
+	int sum = 0;
+	
+	if(head == NULL)
+	{
+		return (0);
+	}
+	while (list_h != NULL)
+	{
+		sum += list_h->n;
+		list_h = list_h->next;
+	}
+	return (sum);
+}
