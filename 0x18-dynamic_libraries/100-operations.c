2c5c83c83acb1e0eabb50e4108cf4e6fc526d4d5
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-18 07:24:11 -0700

    python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+* add - addition
+* @a: input
+* @b: input
+* Return: int
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtraction
+* @a: input
+* @b: input
+* Return: int
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplication
+* @a: input
+* @b: input
+* Return: int
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - division
+* @a: input
+* @b: input
+* Return: int
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  mod - modulus
+  @a: input
+  @b: input 
+  Return: int
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
