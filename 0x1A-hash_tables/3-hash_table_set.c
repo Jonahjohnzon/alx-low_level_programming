589516a8138329c8c8a4bc3d703848a059be7027
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-09 03:35:50 -0700

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	if (value_ == NULL)
+	{
 		return (0);
-
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
