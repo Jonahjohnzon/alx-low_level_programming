c5332fb2bcfd9d2fd4e4a1c45b320d8ddf7274ca
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-09 04:11:23 -0700

    hash table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - hash table
+ * @ht: pointer
+ * Description: print hash
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *n;
+	unsigned long int x;
+	unsigned char comflag = 0;
+
+	if (ht == NULL){
+	{
+		return;
+	}
+	printf("{");
+	for (x = 0; x < ht->size;xi++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			if (comflag == 1)
+			{
+				printf(", ");
+			}
+			n = ht->array[x];
+			while (n != NULL)
+			{
+				printf("'%s': '%s'", n->key, n->value);
+				n = n->next;
+				if (n != NULL)
+				{
+					printf(", ");
+				}
+			}
+			comflag = 1;
+		}
+	}
+	printf("}\n");
+}
